ea416e1aaf41e502a73e7df5f1d3d932ad84db18
tsoushi <79128170+tsoushi@users.noreply.github.com>
2023-04-23 23:13:09 +0900

    expressの基本部分だけ作成

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
+
+
+const prisma = new PrismaClient();
+
+const app = express();
+
+app.use(express.json());
+
+
+app.get('/user', async (req, res) => {
+    const users = await prisma.user.findMany();
+
+    res.json({users: users});
+});
+
+app.post('/user', async (req, res) => {
+    if (typeof req.body.email === 'string') {
+        prisma.user.create({
+            data: {
+                email: req.body.email
+            }
+        })
+            .then((user) => {
+                res.json({status: 'ok', user: user});
+            })
+            .catch((e) => {
+                res.json({status: 'failed', message: 'Error: failed to create user'});
+            })
+
+    } else {
+        res.json({status: 'failed', message: 'Error: `email` property is required'});
+    }
+});
+
+app.listen(8000, '0.0.0.0');
